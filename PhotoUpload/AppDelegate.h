56ddd6a4b791f6b61f9c5239a6315f76d6a99d4b
administrator <administrator@mdm2.maruhachi.co.jp>
2015-06-23 14:29:21 +0900

    Initial Commit

---
+++ PhotoUpload/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  PhotoUpload
+//
+//  Created by administrator on 2015/06/23.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ PhotoUpload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhotoUpload
+//
+//  Created by administrator on 2015/06/23.
+//  Copyright (c) 2015年 administrator. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
